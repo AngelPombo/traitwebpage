37737e9534a0c2e0a8a534cd425154306a8c994c
Angel <angelpombo.92@gmail.com>
2024-04-16 13:34:27 +0200

    inicio back y base de datos

---
+++ server/src/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const fileupload = require('express-fileupload');
+const path = require('path');
+
+const cors = require('cors');
+const server = express();
+
+server.use(cors());
+server.use(express.json())
+/* server.use(express.urlencoded({extended: false})); */
+server.use(fileupload());
+
+server.use((err, _req, res, _next) => {
+    console.log("ERROR: " + err.message);
+    res.status(err.code ?? 500);
+    res.send({
+        status: "Error",
+        message: `${err.message}`});
+});
+
+server.use((_req, res) => {
+    res.status(404);
+    res.send("Página no encontrada");
+});
+
+module.exports = server;
---
+++ server/src/database/scriptdb.js
@@ -0,0 +1,94 @@
+"use strict";
+
+const {createDBconnection} = require('./db');
+
+require('dotenv').config();
+
+const dbName = process.env.DB_NAME;
+
+const adminPass = process.env.ADMIN_PASS;
+
+async function createDB(){
+
+    try{
+        const pool = await createDBconnection();
+
+        await pool.query(`DROP DATABASE IF EXISTS ${dbName}`);
+
+        await pool.query(`CREATE DATABASE ${dbName}`)
+
+        await pool.query(`USE ${dbName}`);
+
+        await pool.query(
+
+            `
+            CREATE TABLE IF NOT EXISTS admins
+            (
+                id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                admin_name VARCHAR(40) UNIQUE NOT NULL,
+                pwd VARCHAR(512) NOT NULL,
+                create_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+            );
+            `
+        );
+
+        await pool.query(
+
+            `
+            CREATE TABLE IF NOT EXISTS events
+            (
+                id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                create_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                last_update DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                edited BOOLEAN DEFAULT FALSE,
+                title VARCHAR(100) NOT NULL,
+                content VARCHAR(2000) NOT NULL,
+                event_type ENUM('Tipo 1', 'Tipo 2') NOT NULL
+            );
+            `
+        );
+
+        await pool.query(
+            
+            `
+            CREATE TABLE IF NOT EXISTS events_photos
+            (
+                id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                photo_name VARCHAR(500),
+                photo_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                event_id INT UNSIGNED NOT NULL,
+                FOREIGN KEY (event_id) REFERENCES events(id)
+            );
+            `
+        );
+
+        await pool.query(
+
+            `
+            CREATE TABLE IF NOT EXISTS carrousel_photos
+            (
+                id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                photo_name VARCHAR(500),
+                photo_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+            )
+            `
+        )
+
+
+        await pool.query(
+            `INSERT INTO admins (id, admin_name, pwd, create_date) VALUES (DEFAULT, 'admin', SHA2('${adminPass}', 512), DEFAULT)`
+        );
+
+        console.log(`Si existía una base de datos con el mismo nombre se ha eliminado. Además, se ha creado una nueva base de datos con el nombre "${dbName}" y sus correspondientes tablas.`);
+
+        process.exit(0);
+
+    } catch(e){
+
+        console.log(e)
+        process.exit(1);
+    }
+
+}
+
+createDB();
